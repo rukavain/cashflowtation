1a5526ffda1ea72872b586b35ad8c3752ecae6be
rukavain <magtoto599@gmail.com>
2025-01-29 16:23:41 +0800

    added account, login, and register pages

--- src/app/account/login/page.tsx
+++ src/app/account/login/page.tsx
@@ -1,28 +1,57 @@
 export default function Login() {
   return (
-    <main className="flex flex-col justify-evenly items-center border-2 rounded-md p-5 h-svh mx-8">
-      <div className="flex flex-col justify-center items-center">
-        <img src="/app-logo.png" alt="" />
-        <h1 className="font-thin text-gray-600 text-xl">
-          Your wallet, organized.
-        </h1>
+    <main className="flex flex-col justify-between items-center border-2 rounded-md p-5 h-svh mx-4">
+      <div className="self-start">
+        <div className="border-2 border-gray-300 min-w-max p-2 rounded-xl">
+          <img className="h-6" src="/back-icon.png" alt="" />
+        </div>
       </div>
-      <div className="flex justify-center items-center w-full max-w-xl flex-col gap-4">
-        <button className=" bg-gray-950 text-white font-thin text-xl rounded-md py-3 px-4 w-full max-w-md">
-          Sign up
-        </button>
-
-        <h1 className="text-sm text-gray-600 font-thin">
-          Already have an account?
-        </h1>
-        <button className=" border-2 border-gray-700 text-gray-950 font-thin text-xl rounded-md py-3 px-4 w-full max-w-md">
-          Log in
-        </button>
+      <h1 className="text-4xl font-semibold text-left">
+        Welcome back! Glad to see you, Again
+      </h1>
+      <div className="gap-5 flex flex-col justify-center items-center w-full ">
+        <div className="flex-1 w-full gap-3 flex flex-col">
+          <input
+            type="email"
+            className="border border-gray-400 rounded-md bg-gray-300 w-full py-4 px-3"
+            placeholder="Enter your email"
+          />
+          <input
+            type="password"
+            className=" border border-gray-400 rounded-md bg-gray-300 w-full py-4 px-3"
+            placeholder="Enter your password"
+          />
+          <h1 className="self-end text-sm text-gray-600">Forgot password?</h1>
+          <button className=" bg-gray-900  text-white font-thin text-xl rounded-md p-4 w-full max-w-md">
+            Log in
+          </button>
+        </div>
+        <div className="flex flex-col w-full justify-center items-center gap-6">
+          <div className="w-full flex justify-center items-center gap-2">
+            <div className="border-t flex-1 border-gray-500"></div>
+            <h1 className="text-sm  font-semibold text-gray-600">
+              Or Login with
+            </h1>
+            <div className="border-t flex-1 border-gray-500"></div>
+          </div>
+          <div className="w-full flex gap-4 justify-center items-center">
+            <div className="flex-1 border border-gray-400 rounded-md p-2 flex justify-center items-center">
+              <img
+                className="h-10 flex justify-center items-center"
+                src="/google-icon.png"
+                alt=""
+              />
+            </div>
+            <div className="flex-1 border border-gray-400 rounded-md p-2 flex justify-center items-center h-full">
+              <img className="h-10 " src="/github-icon.png" alt="" />
+            </div>
+          </div>
+        </div>
       </div>
       <div>
-        <p className="text-sm text-center text-gray-600">
-          By using this app, you agree to Cashflowtations Terms of Use and
-          Privacy Policy.
+        <p className="text-sm">
+          Don't have an account?{" "}
+          <span className="font-bold"> Register now</span>
         </p>
       </div>
     </main>
--- src/app/account/register/page.tsx
+++ src/app/account/register/page.tsx
@@ -1,3 +1,68 @@
 export default function Register() {
-  return <h1>Register page</h1>;
+  return (
+    <main className="flex flex-col justify-between items-center border-2 rounded-md p-5 h-svh mx-4">
+      <div className="self-start">
+        <div className="border-2 border-gray-300 min-w-max p-2 rounded-xl">
+          <img className="h-6" src="/back-icon.png" alt="" />
+        </div>
+      </div>
+      <h1 className="text-4xl font-semibold text-left">
+        Hello! Register to get started
+      </h1>
+      <div className="gap-5 flex flex-col justify-center items-center w-full ">
+        <div className="flex-1 w-full gap-3 flex flex-col">
+          <input
+            type="text"
+            className="border border-gray-400 rounded-md bg-gray-300 w-full py-4 px-3"
+            placeholder="Username"
+          />
+          <input
+            type="email"
+            className="border border-gray-400 rounded-md bg-gray-300 w-full py-4 px-3"
+            placeholder="Email"
+          />
+          <input
+            type="password"
+            className="border border-gray-400 rounded-md bg-gray-300 w-full py-4 px-3"
+            placeholder="Password"
+          />
+          <input
+            type="password"
+            className="border border-gray-400 rounded-md bg-gray-300 w-full py-4 px-3"
+            placeholder="Confirm password"
+          />
+          <h1 className="self-end text-sm text-gray-600">Forgot password?</h1>
+          <button className=" bg-gray-900  text-white font-thin text-xl rounded-md p-4 w-full max-w-md">
+            Log in
+          </button>
+        </div>
+        <div className="flex flex-col w-full justify-center items-center gap-6">
+          <div className="w-full flex justify-center items-center gap-2">
+            <div className="border-t flex-1 border-gray-500"></div>
+            <h1 className="text-sm  font-semibold text-gray-600">
+              Or Login with
+            </h1>
+            <div className="border-t flex-1 border-gray-500"></div>
+          </div>
+          <div className="w-full flex gap-4 justify-center items-center">
+            <div className="flex-1 border border-gray-400 rounded-md p-2 flex justify-center items-center">
+              <img
+                className="h-10 flex justify-center items-center"
+                src="/google-icon.png"
+                alt=""
+              />
+            </div>
+            <div className="flex-1 border border-gray-400 rounded-md p-2 flex justify-center items-center h-full">
+              <img className="h-10 " src="/github-icon.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <p className="text-sm">
+          Already have an account? <span className="font-bold"> Login now</span>
+        </p>
+      </div>
+    </main>
+  );
 }
